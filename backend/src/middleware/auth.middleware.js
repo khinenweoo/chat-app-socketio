55d93bfc8fa3a4859ef0029ac05b5cf4823eb77c
khinenweoo <devkhinenweoo@gmail.com>
2025-06-24 16:24:28 +0700

    add features: jwt authentication, signup, login and update profile

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -14,11 +14,22 @@ export const protectRoute = async (req, res, next) => {
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
         if (!decoded) {
-            return res.status(401).json({ message: "Unauthorized - Invalix Token" });
+            return res.status(401).json({ message: "Unauthorized - Invalid Token" });
         }
 
+        // find user in db and select everything except password
+        const user = await User.findById(decoded.userId).select("-password");
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        req.user = user;
+
+        next()
 
     } catch (error) {
-
+        console.log("Error in protectRoute middleware: ", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
     }
 }
