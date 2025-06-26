d256c157a57bd99c3f9132c56b5a85883364204f
khinenweoo <devkhinenweoo@gmail.com>
2025-06-27 00:08:11 +0700

    fix bug

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,44 +1,42 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import { connectDB } from "./lib/db.js";
-import cors from "cors";
 import { app, server } from "./lib/socket.js";
-import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-// to be able to extract json data out of request body , use this middleware
-// Increase the limit for Json and url encoded data
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
     credentials: true,
-}));
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// Note: serving the API and react applications under same place
-if(process.env.NODE_ENV === "production") {
-    // use static middleware to just put our path to static assets
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    // if we visit any routes, we like to see our react app
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 server.listen(PORT, () => {
-    console.log("server is running on port: "+ PORT);
-    connectDB();
-})
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
